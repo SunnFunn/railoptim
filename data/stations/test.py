0b5b2844efdc9a2bc0d2c57477edbf78066d1507
Aleksey Tretyakov <tretyakov_av@rusagrotrans.ru>
2026-05-24 16:55:03 +0300

    minors

--- data/stations/test.py
+++ data/stations/test.py
@@ -1,6 +1,8 @@
 import pandas as pd
 
 
-df = pd.read_parquet("stations_nsi_raw.parquet")
+# df = pd.read_parquet("stations_nsi_raw.parquet")
+# df = pd.read_parquet("sbin_esr_index.parquet")
+df = pd.read_parquet("osm_esr_index.parquet")
 df.to_excel("test.xlsx", index=False)
 print("файл сохранен")
